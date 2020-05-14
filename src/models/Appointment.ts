878dfbd7be1978e8b0c40dbd87a0b985466bf4bf
André Silveira <andrellsilveira@gmail.com>
2020-05-14 18:51:00 -0300

    Inicio de refatoraçao para uso de banco de dados

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,23 +1,44 @@
-import { uuid } from 'uuidv4';
+// Ao utilizar entity do TypeORM a geração do UUID é realizada pelo Decorator
+// import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * O Decorator "@Entity" é utilizado para indicar a qual tabela esse model faz referência.
+ * */
+@Entity('appointments')
 class Appointment {
+    /**
+     * O Decorator "@PrimaryGeneratedColumn" indica que o atributo é uma chave da tabela e deve
+     * ser gerado automaticamente conforme o parâmetro especificado
+     */
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    /**
+     * O Decorator "@Column" indica que o atributo é uma coluna da tabela
+     * Se não for passado nenhum parâmetro para o Decorator, assumirá que
+     * a coluna é do tipo varchar
+     */
+    @Column()
     provider: string;
 
+    @Column('time with time zone')
     date: Date;
 
-    /**
+    /*
      * A função helper "Omit" permite indicar os atributos da classe que não poderão ser passados
      * como parâmetro para o método, nesse caso o próprio construtor da classe.
      * @param data
      */
     /** Aqui é possível desestruturar: constructor({ provider, date }: Omit<Appointment, 'id'>) { */
+    /*
+    Ao utilizar Decorators não é necessário especificar um construtor para a classe
     constructor(data: Omit<Appointment, 'id'>) {
         this.id = uuid();
         this.provider = data.provider;
         this.date = data.date;
     }
+    */
 }
 
 export default Appointment;
--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,37 +1,44 @@
-import { isEqual } from 'date-fns';
+// import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 
-interface CreateRepositoryDTO {
-    provider: string;
-    date: Date;
-}
-
-class AppointmentsRepository {
-    private appointments: Appointment[];
-
-    constructor() {
-        this.appointments = [];
-    }
-
-    /**
-     * Retorna todos os agendamentos
-     */
-    public all(): Appointment[] {
-        return this.appointments;
-    }
-
+/**
+ * O Decorator "@EntityRepository" indica qual é o model ao qual esse repositório faz
+ * referência, conforme o parâmetro especificado para o mesmo
+ */
+@EntityRepository(Appointment)
+/**
+ * Ao herdar a classe "Repository" temos a disposição vários métodos para manipulação dos
+ * registros do model especificado como parâmetro
+ */
+class AppointmentsRepository extends Repository<Appointment> {
     /**
      * Método que buscará um objeto a apartir de uma data
      * Retorno: Appointment ou nulo
-     * @param date 
+     * @param date
      */
-    public findByDate(date: Date): Appointment | null {
+    public async findByDate(date: Date): Promise<Appointment | null> {
         /**
          * Realiza pesquisa dentro do array para verificar se já existe um objeto com a mesma data
          * do agendamento que está sendo criado
          */
-        const findAppointment = this.appointments.find(appointment => isEqual(date, appointment.date));
+        /* const findAppointment = this.appointments.find(appointment =>
+            isEqual(date, appointment.date),
+        ); */
+
+        /**
+         * O método "findOne" da classe "Repository" busca por um registro específico,
+         * conforme os parâmetros especificados
+         */
+        const findAppointment = await this.findOne({
+            /**
+             * Busca onde o campo "date" da tabela "appointments" seja igual ao parâmetros
+             * "date" passado para o método
+             * A forma abaixo foi reduzida devido ao nome da coluna ser o mesmo do parâmetro
+             */
+            where: { date },
+        });
 
         /**
          * Retorna o objeto encontrado ou (||) nulo
@@ -40,18 +47,10 @@ class AppointmentsRepository {
     }
 
     /**
-     * Método que realiza a criação de um Appointment
-     * @param provider 
-     * @param date 
+     * INFORMAÇÃO!! O método "findByDate" por ser assíncrono sempre retornará uma Promisse,
+     * assim, é possível utlizá-lo com o método "then", o qual retornará uma resposta após
+     * finalizado, por exemplo: findByDate('00/00/0000').then(response => ...);
      */
-    public create(data: /** Aqui é possível desestruturar: { provider, date } */ CreateRepositoryDTO): Appointment {
-        const appointment = new Appointment(data);
-        /** Utilizando com desestruturação: const appointment = new Appointment(provider, date); */
-
-        this.appointments.push(appointment);
-
-        return appointment;
-    }
 }
 
 export default AppointmentsRepository;
