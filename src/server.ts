8679c4c35aad453a9c876d100670db9d5dabf12e
André Silveira <andrellsilveira@gmail.com>
2020-07-14 19:08:43 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 /** A importação desse complemento deve ser realizada logo após a importação do Express */
 import 'express-async-errors';
 import './database';
@@ -11,6 +12,7 @@ import handlerErrors from './middlewares/handlerErrors';
 const porta = 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 /** Define uma rota para visulização dos arquivos de forma estática */
 app.use('/files', express.static(uploadConfig.directory));
